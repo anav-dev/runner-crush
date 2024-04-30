7b7e05a4fbe81ccae66b6aff6de90051f2b8d8b1
ana <ana@blueberry.ie>
2024-04-30 12:36:33 +0100

    js: add dropdown functionality to script.js and fix dropdown boxes issues on small screens

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,45 @@
 /*** Index Page ***/
-// 1. create dropdown box function
+
+// 1. open dropdown box
+function openDropdownInstructions()
+{
+    //console.log('clicked')
+    const dropdownInstructions = document.getElementById('dropdownInstructions').classList.toggle('show');
+
+    // if dropdown is displayed, hide arrow icon and show x mark
+    const arrow = document.getElementsByClassName('fa-angle-down')[0]
+    const xmark = document.getElementsByClassName('fa-xmark')[0]
+
+    if(dropdownInstructions)
+    {
+        arrow.style.display = 'none'
+        xmark.style.display = 'inline-block'
+    } else
+    { 
+        xmark.style.display = 'none'
+        arrow.style.display = 'inline-block'
+    }    
+}
+
+function openDropdownFaq()
+{
+    const dropdownFaq = document.getElementById('dropdownFaq').classList.toggle('show')
+
+    // if dropdown is displayed, hide arrow icon and show x mark
+    const arrow = document.getElementsByClassName('fa-angle-down')[1]
+    const xmark = document.getElementsByClassName('fa-xmark')[1]
+
+    if(dropdownFaq)
+    {
+        arrow.style.display = 'none'
+        xmark.style.display = 'inline-block'
+    } else
+    { 
+        xmark.style.display = 'none'
+        arrow.style.display = 'inline-block'
+    }    
+}
+
 
 
 /*** Game Page ***/
