77c455630c0e6c74b7b99756a9c580cbbb4392e8
ana <ana@blueberry.ie>
2024-04-30 10:24:09 +0100

    pages: add game.html page structure and its responsive design, and add script.js comments structure

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+/*** Index Page ***/
+// 1. create dropdown box function
+
+
+/*** Game Page ***/
+/* Code inspiration source: Ania Kubow tutorial */
+
+// 1. create game board 
+  
+// 2. create function to drag objects
+
+// 3. create function to drop objects
+
+// 4. create function to check objects match
+
+// 5. create function to clear game, start over
